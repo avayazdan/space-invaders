3ec3a0957c612c08e7e89c5cb147015e1916a8b2
Ava Yazdany <avayaz1337@gmail.com>
2022-05-25 21:41:30 +0200

    shooter movement added

--- app.js
+++ app.js
@@ -25,3 +25,45 @@ function drawAliens() {
   aliens.forEach((alien) => squares[alien].classList.add('alien'))
 }
 drawAliens()
+
+// shooter variables
+
+let shooterIndex = 188
+const audioShoot = new Audio('audio/shoot.mp3')
+
+// adding SHOOTER
+
+function drawShooter() {
+  squares[shooterIndex].classList.add('shooter')
+}
+drawShooter()
+
+// moving shooter
+
+function moveShooter(e) {
+  console.log(e)
+  switch (e.key) {
+    case 'ArrowLeft':
+      console.log('left arrow')
+      if (shooterIndex > 182) {
+        squares[shooterIndex].classList.remove('shooter')
+        squares[shooterIndex - 1].classList.add('shooter')
+        shooterIndex -= 1
+      }
+      break
+    case 'ArrowRight':
+      console.log('right arrow')
+      if (shooterIndex < 195) {
+        squares[shooterIndex].classList.remove('shooter')
+        squares[shooterIndex + 1].classList.add('shooter')
+        shooterIndex += 1
+      }
+      break
+    case ' ':
+      console.log('space bar')
+      audioShoot.play()
+      squares[shooterIndex - width].classList.add('bullet')
+  }
+}
+
+document.addEventListener('keydown', moveShooter)
